b699351f35b542f7f31568e6743a81c0da63504c
Cate <24cwessels@pinewood.edu>
2023-10-06 10:59:01 -0700

    data formatting
    Formatted data into table (now including tags query)

--- firebase.js
+++ firebase.js
@@ -41,25 +41,7 @@ getSummerCamps().then(results => {
   };
 
   for (var i = 0; i < results.length; i++) {
-    element = document.getElementById("add-camps"); //where database will be added
-
-    div1 = document.createElement("div"); //creating col div
-    div1.classList.add("col"); // adding "col" class to div
-
-    div2 = document.createElement("div"); //creating col div
-    div2.classList.add("col"); // adding "col" class to div
-
-    div3 = document.createElement("div"); //creating col div
-    div3.classList.add("col"); // adding "col" class to div
-
-    a = document.createElement("a"); // creating a for name
-    a.classList.add("p-3")
-
-    org = document.createElement("div"); // creating div for organization name
-    org.classList.add("p-3")
-
-    part = document.createElement("div"); // creating div for # of participants
-    part.classList.add("p-3")
+    [element, div1, div2, div3, a, org, part] = formatData(i)
 
     //getting data 
     a.innerHTML = results[i].name; 
@@ -71,8 +53,36 @@ getSummerCamps().then(results => {
     div2.appendChild(org);
     div3.appendChild(part);
     element.append(div1, div2, div3);
+
   }
 });
+
+function formatData(i) {
+  element = document.getElementById("add-camps"); //where database will be added
+
+  div1 = document.createElement("div"); //creating col div
+  div1.classList.add("col"); // adding "col" class to div
+
+  div2 = document.createElement("div"); //creating col div
+  div2.classList.add("col"); // adding "col" class to div
+
+  div3 = document.createElement("div"); //creating col div
+  div3.classList.add("col"); // adding "col" class to div
+
+  a = document.createElement("button"); // creating a for name
+  a.classList.add("p-3")
+  a.classList.add("btn")
+  a.classList.add("btn-outline-success")
+  a.classList.add("summerCampButton")
+
+  org = document.createElement("div"); // creating div for organization name
+  org.classList.add("p-3")
+
+  part = document.createElement("div"); // creating div for # of participants
+  part.classList.add("p-3")
+
+  return [element, div1, div2, div3, a, org, part];
+}
 
 function setUpFirebaseDatabase() {
   // add collection for Summer Program 1
@@ -144,11 +154,18 @@ function showTag(tag) {
     };
 
     for (var i = 0; i < results.length; i++) {
+      [element, div1, div2, div3, a, org, part] = formatData(i)
 
-      element = document.getElementById("add-camps");
-      li = document.createElement("li");
-      li.innerHTML = "<strong> Name: </strong>" + results[i].name + "<strong> Organization: </strong>" + results[i].organization + "<strong> Link: </strong>" + results[i].link + "<strong> tags: </strong>" + results[i].tags + "<strong> Participated: </strong>" + results[i].participated.length.toString() + "<strong> Comments: </strong>" + results[i].comments + "<strong> status: </strong>" + results[i].status;
-      element.appendChild(li);
+      //getting data 
+      a.innerHTML = results[i].name; 
+      org.innerHTML = results[i].organization;
+      part.innerHTML = results[i].participated;
+  
+      //append HTML 
+      div1.appendChild(a);
+      div2.appendChild(org);
+      div3.appendChild(part);
+      element.append(div1, div2, div3);
     }
   });
 }
