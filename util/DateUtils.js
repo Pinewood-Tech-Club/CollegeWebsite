0aaa31af3aac4e529d36d995f0033902c989000d
brigerodev <62488289+lolhol@users.noreply.github.com>
2023-11-10 15:52:03 -0800

    add date conversion util in js

---
+++ util/DateUtils.js
@@ -0,0 +1,7 @@
+function convertGradeToDate(grade) {
+  var currentTime = new Date();
+  var add = currentTime.getMonth() > 6 ? 1 : 0;
+  var starting = 12 - grade + add;
+  var year = currentTime.getFullYear();
+  return year + starting;
+}
