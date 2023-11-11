cae3567d9053d24c86eb9f68a3778074141a28a9
brigerodev <62488289+lolhol@users.noreply.github.com>
2023-11-10 16:09:18 -0800

    add closest word to from list (for auto complete)

--- util/DateUtils.js
+++ util/DateUtils.js
@@ -1,4 +1,4 @@
-function convertGradeToDate(grade) {
+export function convertGradeToDate(grade) {
   var currentTime = new Date();
   var add = currentTime.getMonth() > 6 ? 1 : 0;
   var starting = 12 - grade + add;
