9a03556e6f1e42318c1b88dc6780d75bb5a7f17e
Tony <tonywu3758@gmail.com>
2024-02-01 22:09:57 -0800

    Actually sorts by participants now
    didnt take that long I can actually get sleep

--- firebase.js
+++ firebase.js
@@ -22,7 +22,16 @@ function getSummerCamps() {
 
 //uses results to append into "add-camps" id
 getSummerCamps().then(results => {
-
+  //i like insertion sort ok dont judge
+  for (var i = 1; i < results.length; i++){
+    var idx = i;
+    while (idx > 0 && results[idx].participated.length > results[idx-1].participated.length){
+      var temp = results[idx]
+      results[idx] = results[idx-1];
+      results[idx-1] = temp;
+      idx--;
+    }
+  }
   element = document.getElementById("add-camps");
 
   while (element.hasChildNodes()) {
@@ -42,8 +51,10 @@ getSummerCamps().then(results => {
     div2.appendChild(org);
     div3.appendChild(part);
     element.append(div1, div2, div3);
-
   }
+  
+
+
 });
 
 //simplifies prev. function code
@@ -266,7 +277,20 @@ function showTag(tag) {
     while (element.hasChildNodes()) {
       element.firstChild.remove()
     };
+<<<<<<< Updated upstream
 
+=======
+    //simple insertion sort (ik built-in sort is faster but im too lazy to write custom comparator function so cry abt it)
+    for (var i = 1; i < results.length; i++){
+      var idx = i;
+      while (idx > 0 && results[idx].participated.length > results[idx-1].participated.length){
+        var temp = results[idx]
+        results[idx] = results[idx-1];
+        results[idx-1] = temp;
+        idx--;
+      }
+    }
+>>>>>>> Stashed changes
     for (var i = 0; i < results.length; i++) {
       [element, div1, div2, div3, a, org, part] = formatData(i)
 
@@ -320,4 +344,160 @@ function createFromAddContent() {
   for (var i = 0; i < littleArrayHeeHee.length; i++) {
     littleArrayHeeHee[i].value = ""
   }
+<<<<<<< Updated upstream
+=======
+}
+
+function adminEdit() {
+  console.log("admin is editing");
+  addGeneralDescription = document.getElementById("addGeneralDescription");
+  addWebLink = document.getElementById("addWebLink");
+  AdminParticipantEdit = document.getElementById("AdminParticipantEdit");
+  currentParticipants = document.getElementById("addParticipants");
+  commentSection = document.getElementsByClassName("userComment");
+  modalFooter = document.getElementById("modalFooter");
+  editContent = document.getElementById("editContent");
+  tagOne = document.getElementById("tag0");
+  tagTwo = document.getElementById("tag1");
+  tagThree = document.getElementById("tag2");
+
+  AdminParticipantEdit.style.display = "flex";
+  editContent.style.display = "none";
+
+  console.log(tagOne);
+
+  tagOne.innerHTML = '<input class="form-control form-control-sm" value='+ tagOne.innerText+'>';
+  tagTwo.innerHTML = '<input class="form-control form-control-sm" value='+ tagTwo.innerText+'>';
+  tagThree.innerHTML = '<input class="form-control form-control-sm" value='+ tagThree.innerText+'>';
+  
+  addGeneralDescription.innerHTML = '<textarea class="form-control" id="floatingTextarea">'+addGeneralDescription.innerText + '</textarea>';
+  addWebLink.innerHTML = '<input class="form-control" value='+addWebLink.innerText+'>';
+  
+  rowContainer = document.createElement("div");
+  rowContainer.classList.add("container");
+  rowContainer.classList.add("px-3");
+  rowContainer.classList.add("text-center");
+  rowContainer.classList.add("contentModal");
+  rowContainer.classList.add("contentContainer");
+
+  console.log(currentParticipants.children.length/2);
+
+  console.log("editing children");
+  rowDiv = document.createElement("div");
+  rowDiv.classList.add("row");
+  rowDiv.classList.add("gx-3");
+  rowDiv.classList.add("row-cols-1");
+  rowDiv.classList.add("g-3");
+
+  
+
+  // if (rowDiv.children.length == currentParticipants.children.length/2) {
+    
+  // } else {
+      for (var i = 0; i < (currentParticipants.children.length/2); i++) { 
+        
+        empty = document.createElement("p");
+        rowContent = document.createElement("button");
+
+        rowContent.classList.add("btn-close");
+        //rowContent.classList.add("g-3");
+        rowContent.setAttribute("aria-label", "Close");
+        rowContent.setAttribute("type", "button");
+        rowContent.setAttribute("onClick", `deleteParticipant(${i})`);
+
+        rowDiv.appendChild(empty);
+        empty.appendChild(rowContent);
+      }
+  // }
+  rowContainer.appendChild(rowDiv);
+  AdminParticipantEdit.appendChild(rowContainer);
+
+  // console.log(rowDiv.children.length);
+  // console.log(rowDiv);
+
+  for (var c = 0; c < (commentSection.length); c++) {
+    //console.log(commentSection[c].innerText);
+    commentSection[c].innerHTML = '<textarea class="form-control" id="floatingTextarea">'+commentSection[c].innerText + '</textarea>';
+  }
+
+  doneButton = document.createElement("button");
+  doneButton.classList.add("btn");
+  doneButton.classList.add("btn-outline-success");
+  doneButton.setAttribute("onClick", "exitEdit()");
+  doneButton.setAttribute("id");
+  modalFooter.appendChild(don, "doneButton");
+  doneButton.innerHTML = "Exit Editing";
+};
+
+function exitEdit() {
+  addGeneralDescription = document.getElementById("addGeneralDescription");
+  updateCommentSection = document.getElementById("addComments");
+  tags = document.getElementById("addTagsModal");
+  webLink = document.getElementById("addWebLink");
+  webValue = webLink.children[0].value;
+  modalHeader = document.getElementById("modal-header");
+  campName = modalHeader.children[0].textContent;
+
+  existingCommentsArray = [];
+  existingTagsArray = [];
+
+  for (var i = 0; i < updateCommentSection.children.length; i++) { 
+    if (updateCommentSection.children[i].children[0].value != "") {
+        existingCommentsArray.push(updateCommentSection.children[i].children[0].value);
+      }
+    };
+
+  for (var i = 0; i < tags.children.length; i++) { 
+     existingTagsArray.push(tags.children[i].children[0].value);
+  };
+
+  console.log(existingCommentsArray);
+  console.log(existingTagsArray);
+  
+  db.collection("college-counseling-database").doc(campName).update({
+    description: addGeneralDescription.children[0].value,
+    tags: existingTagsArray,
+    comments: existingCommentsArray, 
+    link: webValue
+    });
+
+  editContent = document.getElementById("editContent");
+  editContent.style.display = "flex";
+
+  doneButton = document.getElementById("doneButton");
+  doneButton.style.display = "none";
+
+  content(campName);
+};
+
+function deleteParticipant(c) {
+  modalHeader = document.getElementById("modal-header");
+  campName = modalHeader.children[0].textContent;
+
+  getSummerCamps().then(results => {
+    let tag = 0;
+
+    for (var i = 0; i < results.length; i++) { 
+      if (results[i] == campName) {
+        tag = i;
+      } else {
+        console.log("not a match");
+      }
+    }
+
+    participantList = results[tag].participated;
+
+    console.log(participantList);
+    console.log(c);
+
+    participantList.splice(c, 1);
+    console.log(participantList);
+
+    db.collection("college-counseling-database").doc(campName).update({
+      participated: participantList
+      });
+
+    content(campName);
+  })
+>>>>>>> Stashed changes
 }
