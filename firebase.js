43d8cfef2e6a196074b13108dac779a4ed7fbf0d
Tony <tonywu3758@gmail.com>
2024-02-03 15:54:15 -0800

    insertion sort repost

--- firebase.js
+++ firebase.js
@@ -27,7 +27,15 @@ function getSummerCamps() {
 
 //uses results to append into "add-camps" id
 getSummerCamps().then(results => {
-
+  for (var i = 1; i < results.length; i++){
+    var idx = i;
+    while (idx > 0 && results[idx].participated.length > results[idx-1].participated.length){
+      var temp = results[idx]
+      results[idx] = results[idx-1];
+      results[idx-1] = temp;
+      idx--;
+    }
+  }
   element = document.getElementById("add-camps");
 
   while (element.hasChildNodes()) {
@@ -318,6 +326,16 @@ function showTag(tag) {
     while (element.hasChildNodes()) {
       element.firstChild.remove()
     };
+    //simple insertion sort (ik built-in sort is faster but im too lazy to write custom comparator function so cry abt it)
+    for (var i = 1; i < results.length; i++){
+      var idx = i;
+      while (idx > 0 && results[idx].participated.length > results[idx-1].participated.length){
+        var temp = results[idx]
+        results[idx] = results[idx-1];
+        results[idx-1] = temp;
+        idx--;
+      }
+    }
     for (var i = 0; i < results.length; i++) {
       var flag = false;
       for (var j = 0; j < results[i].tags.length; j++){
